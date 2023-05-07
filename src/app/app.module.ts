183d3069c7a055b3aea559dc4ecd722f669a68a0
montheooo <montheooo@yahoo.fr>
2023-05-07 13:07:59 -0400

    adding header+footer+content layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { BannerComponent } from './banner/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
